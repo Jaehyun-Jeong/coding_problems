1bbbd9fda108e5d046213afcf105266875505cbc
jaehyun-jeong <jj.for.jaehyun@gmail.com>
2024-09-18 01:06:27 +0000

    45. Jump Game II: Fixed

--- 45. Jump Game II.c
+++ 45. Jump Game II.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 
 
+
 int jump(int* nums, int numsSize){
+
+    int i = numsSize - 1;
+    int lastWindow = numsSize - 1;
 
     if(numsSize == 1){
         return 0;
     }
 
-    int i = numsSize - 1;
-
-    while(i >= 0 && nums[i] >= numsSize - i - 1){
+    while(i >= 0){
+        if(nums[i] >= numsSize - i - 1)
+            lastWindow = i;
         i--;
     }
 
     // It ends at nums[0]
-    if(i == 0){
-        return 2;
-    } else if(i == -1){
+    if(lastWindow == 0){
         return 1;
+    } else {
+        return 1 + jump(nums, lastWindow+1);
     }
-
-    return 1 + jump(nums, i+1);
 }
 
 
 int main(void){
 
-    int nums[100] = {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5}, numsSize = 34;
-    //int nums[] = {1,2,3};
-    //int nums[] = {0};
-    //int nums[] = {2,3,1,1,4};
-    //int numsSize = 3;
+    int nums1[] = {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5};
+    int nums1Size = 34;
+    printf("%d\n", jump(nums1, nums1Size));
 
-    printf("%d\n", jump(nums, numsSize));
+    int nums2[] = {1,2,3};
+    int nums2Size = 3;
+    printf("%d\n", jump(nums2, nums2Size));
+
+    int nums3[] = {0};
+    int nums3Size = 1;
+    printf("%d\n", jump(nums3, nums3Size));
+
+    int nums4[] = {2,3,1,1,4};
+    int nums4Size = 5;
+    printf("%d\n", jump(nums4, nums4Size));
 
     return 0;
 }
