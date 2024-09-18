b198fc1d836c71ce0db3d40b9aa0726fe1c63910
jaehyun-jeong <jj.for.jaehyun@gmail.com>
2024-09-18 04:18:11 +0000

    2390. Removeing Stars From a String: first try

---
+++ 2390. Removing Stars From a String.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+char* removeStars(char* s) {
+
+    int i = 0, c, ansCounter;
+    char ans[100001];
+    char* ptrAns;
+
+    while(s[i]){
+        i++;
+    }
+    ansCounter = i;
+    ans[ansCounter--] = '\0';
+
+    while(i--){
+        if(s[i] == '*')
+            c++;
+        else if(c > 0)
+            c--;
+        else
+            ans[ansCounter--] = s[i];
+    }
+
+    ptrAns = &ans[ansCounter+1];
+    return ptrAns;
+}
+
+int main(void){
+
+    char s1[] = "leet**cod*e";
+    printf("%s\n", removeStars(s1));
+    // "lecoe"
+
+    char s2[] = "erase*****";
+    printf("%s\n", removeStars(s2));
+    // ""
+
+    return 0;
+}
